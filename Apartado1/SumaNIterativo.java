a39cecc130fa040ae081ba147e42888308405efb
Estefania <tefidenisco@gmail.com>
2023-02-14 19:00:43 +0100

    Lista la suma Iterativa

---
+++ Apartado1/SumaNIterativo.java
@@ -0,0 +1,17 @@
+public class SumaNIterativo{
+	public static void main(String[]args){
+		int limite=Integer.parseInt(args[0]);
+		int suma=0;
+		int i;
+		for(i=0;i<= limite;i++){
+			suma=suma+i;
+
+
+
+		}
+	
+		System.out.println("La suma es "+suma);
+	}
+
+
+}
